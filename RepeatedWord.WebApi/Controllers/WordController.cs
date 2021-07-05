d12f499a8f0d2c2df46e3943e059a69a93642f1b
Alexander Becerra <alexannder15@hotmail.com>
2021-07-05 18:58:50 -0500

    firts commit

---
+++ RepeatedWord.WebApi/Controllers/WordController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RepeatedWord.WebApi.ViewModels;
+using System;
+using System.Linq;
+
+namespace RepeatedWord.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[Controller]")]
+    public class WordController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetRepeatedWords(string text)
+        {
+            //Convert the string into an array of words  
+            string[] items = text.Split(new char[] {' ', '.', ',', '?', '!', ';', ':'}, StringSplitOptions.RemoveEmptyEntries);
+
+            var model = items
+                .GroupBy(word => word) // Group by word 
+                .Select(x => new WordViewModel // Projection to return
+                {
+                    Word = x.Key,
+                    Count = x.Count()
+                })
+                .OrderByDescending(x => x.Count) // sort by count of repeated words descending
+                .ToList();
+
+            return Ok(model);
+        }
+    }
+}
---
+++ RepeatedWord.WebApi/ViewModels/WordViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace RepeatedWord.WebApi.ViewModels
+{
+    public class WordViewModel
+    {
+        public string Word { get; set; }
+
+        public int Count { get; set; }
+    }
+}
